708f77e27e402475f13bc38a5b2b8a4b19215b5f
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2023-07-25 20:12:15 +0200

    linux support

--- scripts/install.js
+++ scripts/install.js
@@ -5,7 +5,12 @@ const appDir = process.argv[1].replace(/scripts[/\\]install\.js$/, '');
 let createCmd;
 if (process.platform === 'win32') {
   createCmd = (fn) => sh.echo(`@node ${appDir}${fn}.js`).to(`~/bin/${fn}.cmd`);
-}
+} else if (process.platform === 'linux') {
+  createCmd = (fn) => {
+    sh.echo(`#!/usr/bin/env bash\nnode ${appDir}${fn}.js`).to(`~/bin/${fn}`);
+    chmod('u+x', `~/bin/${fn}`);
+  };
+} else process.exit(1);
 sh.ls(appDir)
   .filter((fn) => fn.endsWith('.js'))
   .map((fn) => fn.replace(/\.js$/, ''))
